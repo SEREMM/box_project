7a727355afe3e28dceb4c50d21a2d2dcade06e9f
SEREMM <seol19991@gmail.com>
2023-09-14 15:37:34 -0600

    actualizacion

--- 1_reg_box_data.py
+++ 1_reg_box_data.py
@@ -25,20 +25,20 @@ class Reg_box_data():
 
   def registro(self):
     while True:
-      textos = {'result':'Boxer 1 result (win, loss): ','end_type':'End by ko or decision or rtd: ','end_round':'End round: ','c_f':'Country of fight: ','y_f':'Year of fght: ','m_f':'Month of fight: ','d_f':'Day of fight: ',
-                'boxer1':'Name of boxer1: ','birth_b1':'Birth of boxer1: ','height_b1':'Height of boxer1: ','reach_b1':'Reach of boxer1: ','stance_b1':'Stance of boxer1 (0: orthodox, 1: southpaw): ','region_b1':'Region of boxer1: ','w_b1':'Number of win boxer1: ','wk_b1':'Number of ko wins of boxer1: ','d_b1':'Number of draws of boxer1: ','l_b1':'Number of loss of boxer1: ','lk_b1':'Number of ko loss of boxer1: ','b_b1':'Odds in favor of boxer1 (decimal as prec of 1 as the total): ','wiki_boxer1':'Info wiki boxer1',
-                'boxer2':'Name of boxer2: ','birth_b2':'Birth of boxer2: ','height_b2':'Height of boxer2: ','reach_b2':'Reach of boxer2: ','stance_b2':'Stance of boxer2 (0: orthodox, 1: southpaw): ','region_b2':'Region of boxer2: ','w_b2':'Number of win boxer2: ','wk_b2':'Number of ko wins of boxer2: ','d_b2':'Number of draws of boxer2: ','l_b2':'Number of loss of boxer2: ','lk_b2':'Number of ko loss of boxer2: ','b_b2':'Odds in favor of boxer2 (decimal as prec of 1 as the total): ','wiki_boxer2':'Info wiki boxer2'}
+      textos = {'result':'Boxer 1 result (win, loss): ','endtype':'End by ko or decision or rtd: ','endround':'End round: ','c_f':'Country of fight: ','y_f':'Year of fght: ','m_f':'Month of fight: ','d_f':'Day of fight: ',
+                'boxer1':'Name of boxer1: ','birth_b1':'Birth of boxer1: ','height_b1':'Height of boxer1: ','reach_b1':'Reach of boxer1: ','stance_b1':'Stance of boxer1 (0: orthodox, 1: southpaw): ','region_b1':'Region of boxer1: ','b1_w':'Number of win boxer1: ','b1_wk':'Number of ko wins of boxer1: ','b1_d':'Number of draws of boxer1: ','b1_l':'Number of loss of boxer1: ','b1_lk':'Number of ko loss of boxer1: ','b1_bet':'Odds in favor of boxer1 (decimal as prec of 1 as the total): ','wiki_boxer1':'Info wiki boxer1',
+                'boxer2':'Name of boxer2: ','birth_b2':'Birth of boxer2: ','height_b2':'Height of boxer2: ','reach_b2':'Reach of boxer2: ','stance_b2':'Stance of boxer2 (0: orthodox, 1: southpaw): ','region_b2':'Region of boxer2: ','b2_w':'Number of win boxer2: ','b2_wk':'Number of ko wins of boxer2: ','b2_d':'Number of draws of boxer2: ','b2_l':'Number of loss of boxer2: ','b2_lk':'Number of ko loss of boxer2: ','b2_bet':'Odds in favor of boxer2 (decimal as prec of 1 as the total): ','wiki_boxer2':'Info wiki boxer2'}
       data = pd.DataFrame()
       current_key_value = 0
       key_values = list(textos.items())
       while current_key_value < len(key_values):
         if (key_values[current_key_value][0] == 'wiki_boxer1'):
           a,b,c,d = self.reg_2(data['boxer1'], key_values[current_key_value][1])
-          data['texto_b1'], data['summary_b1'], data['len_text_b1'], data['len_summary_b1'] = a,b,c,d
+          data['text_boxer1'], data['summary_boxer1'], data['len_text_boxer1'], data['len_summary_boxer1'] = a,b,c,d
           current_key_value += 1
         elif (key_values[current_key_value][0] == 'wiki_boxer2'):
           a,b,c,d = self.reg_2(data['boxer2'], key_values[current_key_value][1])
-          data['texto_b2'], data['summary_b2'], data['len_text_b2'], data['len_summary_b2'] = a,b,c,d
+          data['text_boxer2'], data['summary_boxer2'], data['len_text_boxer2'], data['len_summary_boxer2'] = a,b,c,d
           current_key_value += 1
         else:
           resp = self.reg_1(key_values[current_key_value][1])
@@ -54,13 +54,13 @@ class Reg_box_data():
 
 
 def create_csv_file():
-    df = pd.DataFrame(columns=['result', 'end_type', 'end_round', 'c_f', 'y_f', 'm_f', 'd_f', 'boxer1',
-       'birth_b1', 'height_b1', 'reach_b1', 'stance_b1', 'region_b1', 'w_b1',
-       'wk_b1', 'd_b1', 'l_b1', 'lk_b1', 'b_b1', 'texto_b1', 'summary_b1',
-       'len_text_b1', 'len_summary_b1', 'boxer2', 'birth_b2', 'height_b2',
-       'reach_b2', 'stance_b2', 'region_b2', 'w_b2', 'wk_b2', 'd_b2', 'l_b2',
-       'lk_b2', 'b_b2', 'texto_b2', 'summary_b2', 'len_text_b2',
-       'len_summary_b2'])
+    df = pd.DataFrame(columns=['result', 'endtype', 'endround', 'c_f', 'y_f', 'm_f', 'd_f', 'boxer1',
+       'birth_b1', 'height_b1', 'reach_b1', 'stance_b1', 'region_b1', 'b1_w',
+       'b1_wk', 'b1_d', 'b1_l', 'b1_lk', 'b1_bet', 'text_boxer1', 'summary_boxer1',
+       'len_text_boxer1', 'len_summary_boxer1', 'boxer2', 'birth_b2', 'height_b2',
+       'reach_b2', 'stance_b2', 'region_b2', 'b2_w', 'b2_wk', 'b2_d', 'b2_l',
+       'b2_lk', 'b2_bet', 'text_boxer2', 'summary_boxer2', 'len_text_boxer2',
+       'len_summary_boxer2'])
     df.to_csv('manual_box_reg.csv', index=False)
 
 def append_csv_row(row):
--- funciones_box_data.py
+++ funciones_box_data.py
@@ -64,7 +64,7 @@ def cross_val(model, X, y, cv=5, figsize=(5,3)):
   train_scores_std = np.std(train_scores, axis=1)
   test_scores_mean = np.mean(test_scores, axis=1)
   test_scores_std = np.std(test_scores, axis=1)
-  
+
   plt.figure(figsize=figsize)
   plt.title("Learning Curve")
   plt.xlabel("Training examples")
@@ -77,99 +77,12 @@ def cross_val(model, X, y, cv=5, figsize=(5,3)):
   plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
            label="Training score")
   plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
-           label="Cross-validation score") 
+           label="Cross-validation score")
   plt.legend(loc="best")
   plt.show()
 
 
-def feat_eng(df):
-  '''
-  Feature engineering values.
-  :df: Dataframe sobre el cual agregar las características.
-  :return: df con las características.
-  '''
-  df = df.copy()
-  suma_peleas_b1 = df.b1_w + df.b1_d + df.b1_l
-  suma_peleas_b2 = df.b2_w + df.b2_d + df.b2_l
-  df['b1_momios_menores'] = np.where(df.b1_bet < df.b2_bet, 1, 0)
-  df['b1_bet_inversa'] = abs(df.b1_bet - 1)
-  df['b2_bet_inversa'] = abs(df.b2_bet - 1)
-  df = df.drop(columns=['b1_bet','b2_bet'])
-  df['b1_mas_peleas'] = np.where(suma_peleas_b1 > suma_peleas_b2, 1, 0)
-  df['b1_menos_peleas'] = np.where(suma_peleas_b1 < suma_peleas_b2, -1, 0)
-  df['b1_mas_win_perc'] = np.where((df.b1_w / suma_peleas_b1) > (df.b2_w / suma_peleas_b2), 1, 0)
-  df['b1_menos_win_perc'] = np.where((df.b1_w / suma_peleas_b1) < (df.b2_w / suma_peleas_b2), -1, 0)
-  df['b1_menos_loss_perc'] = np.where((df.b1_l / suma_peleas_b1) < (df.b2_l / suma_peleas_b2), 1, 0)
-  df['b1_mas_loss_perc'] = np.where((df.b1_l / suma_peleas_b1) > (df.b2_l / suma_peleas_b2), -1, 0)
-  df['b1_mas_ko_perc'] = np.where((df.b1_wk / df.b1_w) > (df.b2_wk / df.b2_w), 1, 0)
-  df['b1_menos_ko_perc'] = np.where((df.b1_wk / df.b1_w) < (df.b2_wk / df.b2_w), -1, 0)
-  df['b1_invicto'] = np.where(df.b1_l <= 0, 1, 0)
-  df['b2_invicto'] = np.where(df.b2_l <= 0, -1, 0)
-  df['b1_local'] = np.where(df.c_f == df.region_b1, 1, 0)
-  df['b2_local'] = np.where(df.c_f == df.region_b2, -1, 0)
-
-  return df
-
-
 from sklearn.base import BaseEstimator, TransformerMixin
-
-
-class Features_encoder(BaseEstimator, TransformerMixin):
-    def __init__(self, encoder1=0, encoder2=0, encoder3=0):
-      '''
-      Vectorizador de string features.
-        :encoder1(defaul 0): vectorizador regiones [c_f, region_b1, region_b2] (if pass encoder, else omit).
-        :encoder2(default 0): vectorizador posturas [stance_b1, stance_b2] (if pass encoder, else omit).
-        :encoder3(default 0): vectorizador estilos [boxstyle_b1, boxstyle_b2] (if pass encoder, else omit).
-      Returns: Dataframe with vectorized values
-      '''
-      self.encoder1 = encoder1
-      self.encoder2 = encoder2
-      self.encoder3 = encoder3
-
-    def fit(self, X, y=None):
-      return self
-
-    def transform(self, X):
-      '''
-      :X: dataframe base.
-      :return: df con las columnas vectorizadas.
-      '''
-      df_1 = X.copy()
-      if self.encoder1 == 0:
-        print('No encoder 1\n')
-      else:
-        columns = ['c_f', 'region_b1', 'region_b2']
-        for i in columns:
-          one_hot_vectors = self.encoder1.transform(df_1[i])
-          feature_names = self.encoder1.get_feature_names_out()
-          temp = pd.DataFrame(one_hot_vectors.toarray(), columns=feature_names, index=df_1.index)
-          df_1 = pd.concat([df_1,temp], axis=1)
-          df_1.drop(columns=i, inplace=True)
-
-      if self.encoder2 == 0:
-        print('No encoder 2\n')
-      else:
-        columns = ['stance_b1', 'stance_b2']
-        for i in columns:
-          one_hot_vectors = self.encoder2.transform(df_1[i])
-          feature_names = self.encoder2.get_feature_names_out()
-          temp = pd.DataFrame(one_hot_vectors.toarray(), columns=feature_names, index=df_1.index)
-          df_1 = pd.concat([df_1,temp], axis=1)
-          df_1.drop(columns=i, inplace=True)
-
-      if self.encoder3 == 0:
-        print('No encoder 3\n')
-      else:
-        columns = ['boxstyle_b1', 'boxstyle_b2']
-        for i in columns:
-          one_hot_vectors = self.encoder3.transform(df_1[i])
-          feature_names = self.encoder3.get_feature_names_out()
-          temp = pd.DataFrame(one_hot_vectors.toarray(), columns=feature_names, index=df_1.index)
-          df_1 = pd.concat([df_1,temp], axis=1)
-          df_1.drop(columns=i, inplace=True)
-
-      return df_1
 
 
 class Data_clusterer(BaseEstimator, TransformerMixin):
@@ -317,16 +230,19 @@ def check_fails_and_probas(df_cluster, y_true, y_pred, prob_loss, prob_win, figs
     temp1['prob_win'] = round(temp1.prob_win, 1)
     temp1 = pd.get_dummies(temp1, columns=['goodpred'])
     temp1 = temp1.groupby(['cluster','prob_win']).sum().reset_index()
-    temp1['false_over_total'] = temp1.goodpred_False / (temp1.goodpred_True + temp1.goodpred_False)
-  
-    fig,ax = plt.subplots(figsize=(figsize))
-    sns.barplot(data=temp1, x='prob_win', y='false_over_total')
-    ax.set_ylim(0,1)
-    plt.xlabel('Prob. Win ex.(0.5 = from 0.46 to 0.55)')
-    plt.ylabel('Perc. false over total')
-    plt.title(f'Perc. false / total. Cluster {i}')
-    plt.show()
-  
+    try:
+        temp1['false_over_total'] = temp1.goodpred_False / (temp1.goodpred_True + temp1.goodpred_False)
+
+        fig,ax = plt.subplots(figsize=(figsize))
+        sns.barplot(data=temp1, x='prob_win', y='false_over_total')
+        ax.set_ylim(0,1)
+        plt.xlabel('Prob. Win ex.(0.5 = from 0.46 to 0.55)')
+        plt.ylabel('Perc. false over total')
+        plt.title(f'Perc. false / total. Cluster {i}')
+        plt.show()
+    except AttributeError:
+        print(f'\n::: Attribute error in cluster: {i} :::\n')
+
   return dfx
 
 
